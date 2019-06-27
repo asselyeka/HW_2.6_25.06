2c3c2547c168c1e613e97070097d82f048493181
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-28 00:07:18 +0600

    Добавьте файлы проекта.

---
+++ HW_2.6_25.06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 6.	Вычислить значение логического выражения при следующих значениях логических величин А, В и С: А = Истина, В = Ложь, С = Ложь:
+a.	А или В; 
+b.	А и В; 
+c.	В или С.
+*/
+
+namespace HW_2._6_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = true;
+            bool B = false;
+            bool C = false;
+
+            Console.Write("а. A или B = "); Console.WriteLine(A || B);
+            Console.Write("b. A и B   = "); Console.WriteLine(A && B);
+            Console.Write("c. B или C = "); Console.WriteLine(B || C);
+
+            Console.ReadKey();
+        }
+    }
+}
